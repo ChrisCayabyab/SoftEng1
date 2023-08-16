e2a034a3b0b7fe207288b5646a01afb1120ee067
ChrisCayabyab <142383617+ChrisCayabyab@users.noreply.github.com>
2023-08-16 15:44:15 +0800

    Create Hello.java

---
+++ Hello.java
@@ -0,0 +1,6 @@
+public class Hello{
+  Public static void manin (String []args){
+    System.out.println("Hello, Software Engineering Course");
+    System.out.println("My name is Cayabyab, Chris Daniel!");
+  }
+}
