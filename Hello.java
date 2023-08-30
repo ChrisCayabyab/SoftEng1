6bae47037a0212767a6a6c89b9d393ce5767c69e
ChrisCayabyab <chris.b.cayabyab@gmail.com>
2023-08-30 17:05:03 +0800

    added a new file browser-support.txt

--- Hello.java
+++ Hello.java
@@ -2,5 +2,6 @@ public class Hello{
   Public static void manin (String []args){
     System.out.println("Hello, Software Engineering Course");
     System.out.println("My name is Cayabyab, Chris Daniel!");
+    System.out.println("That is why developers are highly-paid!");
   }
 }
