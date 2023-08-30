dad7923089872906d6aade52d4112d7b5bf6697f
ChrisCayabyab <chris.b.cayabyab@gmail.com>
2023-08-30 22:25:32 +0800

    modified Hello.java and added MyNewFile.txt

--- Hello.java
+++ Hello.java
@@ -2,6 +2,6 @@ public class Hello{
   Public static void manin (String []args){
     System.out.println("Hello, Software Engineering Course");
     System.out.println("My name is Cayabyab, Chris Daniel!");
-    System.out.println("That is why developers are highly-paid!");
+    System.out.println("Complicated world of SoftDev that is why it pays much!");
   }
 }
